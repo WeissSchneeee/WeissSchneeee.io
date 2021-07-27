4b07784b69ced1ca39f8c84bb789a2888f84b9b3
Weissschneeee <yuanming.huang@student.uts.edu.au>
2021-07-28 02:11:45 +1000

    28/07/2021

---
+++ 进行一个学/index.js
@@ -0,0 +1,7 @@
+window.alert("戴佳伟菜逼")
+
+window.addEventListener('load', function(){
+    const title = document.getElementById('title')
+    console.log(title)
+    title.innerText = "Byebye Byebye" 
+})
